0b7663b62d7a93051349aa33e1b4b85a7a7a027d
Henry <henrygu054@gmail.com>
2020-12-28 16:53:31 -0500

    description change

--- src/About.js
+++ src/About.js
@@ -5,8 +5,8 @@ function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <h2>Self-Motivated, Team-Minded, Punctual</h2>
-      <h3>Student at UWaterloo's Mechatronics Program interested in everything hardware.</h3>
+      <h2>Self-Motivated, Team-Minded, Leader</h2>
+      <h3>Student at UWaterloo's Mechatronics Engineering interested in everything hardware.</h3>
       <img src = "https://i1.wp.com/cdn-9.tubefollow.com/photos/l/384571237189890879/kingjames-up-watching-a-lil-tom-jerry-before-we-head-to-school-384571237189890879.jpg" alt = "leborn jams"/>
     </div>
   );

38be27250a347223610625208ba49ecca46c2727
Henry <henrygu054@gmail.com>
2020-12-28 13:48:50 -0500

    lebron

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ function About() {
       <h1>About Me</h1>
       <h2>Self-Motivated, Team-Minded, Punctual</h2>
       <h3>Student at UWaterloo's Mechatronics Program interested in everything hardware.</h3>
-      
+      <img src = "https://i1.wp.com/cdn-9.tubefollow.com/photos/l/384571237189890879/kingjames-up-watching-a-lil-tom-jerry-before-we-head-to-school-384571237189890879.jpg" alt = "leborn jams"/>
     </div>
   );
 }
